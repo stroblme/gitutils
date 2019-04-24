5ce8f2704c29da1c9fe4f2a60ffaeab337a61c6c
Melvin Strobl <melvin.strobl@web.de>
2019-04-24 17:24:25 +0200

    added cleanupp script

---
+++ git-cleanupp.cmd
@@ -0,0 +1,5 @@
+@echo off
+for %%f in (`git ls-files --exclude-standard --others --directory --ignored`) do (
+	git filter-branch --force --index-filter "git rm --cached --ignore-unmatch '$f'" --prune-empty --tag-name-filter cat -- --all
+	pause
+)
