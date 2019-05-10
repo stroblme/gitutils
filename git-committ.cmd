93c9da645f6b70d205f7b701c6d8f4c4c2fe71ed
m17538 <melvin.strobl@microchip.com>
2019-05-10 14:57:46 +0200

    added trimming whitespaces

--- git-committ.cmd
+++ git-committ.cmd
@@ -7,6 +7,16 @@ for %%m in (%*) do (
    set message=!message! %%m
 )
 
-git commit -m "%message%"
+call :TrimSpaces actual !message!
+
+git commit -m "!actual!"
 git pushh
 git logg
+
+exit
+
+:TrimSpaces
+    setlocal enabledelayedexpansion
+    set Params=%*
+    for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
+    exit /b
