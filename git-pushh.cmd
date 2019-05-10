02fa7a5ac763310adf820675cd4e7b7a619dbd61
m17538 <melvin.strobl@microchip.com>
2019-05-10 11:17:45 +0200

    added simplification for just 1 remote

--- git-pushh.cmd
+++ git-pushh.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 for /F "tokens=2" %%b in ('git branch') do (
     if not x%%b:"\*"==x%%i set _cBranch=%%b
@@ -7,21 +8,29 @@ for /F "tokens=2" %%b in ('git branch') do (
 echo Current branch is %_cBranch%
 
 if %1.==. (
-    set _remotes=git remote
+    set /a _count=0
 
     for /F "tokens=1" %%r in ('git remote') do (
-        echo Shall I push to remote %%r
-        choice /C YNC
-        
-        if ERRORLEVEL==3 (
-            exit
-        ) else if ERRORLEVEL==2 (
-            endlocal 
-        ) else if ERRORLEVEL==1 (
-            echo Good choice
-            git push --set-upstream %%r %_cBranch%
+        set /a _count+=1
+        set _cRemote=%%r
+    )
+    
+    if !_count! gtr 1 (
+        for /F "tokens=1" %%r in ('git remote') do (
+            echo Shall I push to remote %%r
+            choice /C YNC
+            
+            if ERRORLEVEL==3 (
+                exit
+            ) else if ERRORLEVEL==2 (
+                endlocal 
+            ) else if ERRORLEVEL==1 (
+                git push --set-upstream %%r %_cBranch%
+            )
+            
         )
-        
+    ) else (
+        git push --set-upstream !_cRemote! %_cBranch%
     )
 ) else (
     echo Will push to remote %1
