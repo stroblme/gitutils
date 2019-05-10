e872821907274f3181e876ac2e897d7060a7b658
m17538 <melvin.strobl@microchip.com>
2019-05-10 10:41:26 +0200

    improved multi remote pushing and auto branch selection

--- git-pushh.cmd
+++ git-pushh.cmd
@@ -1,14 +1,30 @@
 @echo off
 
-if "%~1"=="" goto remoteProcessing else goto parameterProcessing
-
-:remoteProcessing
-FOR /F %%I IN ('git remote') DO SET var=%%I
-
-for %%i in (%var%) do  (
-    echo %%i
+for /F "tokens=2" %%b in ('git branch') do (
+    if not x%%b:"\*"==x%%i set _cBranch=%%b
 )
 
-:parameterProcessing
-echo %1
-REM git push --set-upstream %1 master
+echo Current branch is %_cBranch%
+
+if %1.==. (
+    set _remotes=git remote
+
+    for /F "tokens=1" %%r in ('git remote') do (
+        echo Shall I push to remote %%r
+        choice /C YNC
+        
+        if ERRORLEVEL==3 (
+            exit
+        ) else if ERRORLEVEL==2 (
+            endlocal 
+        ) else if ERRORLEVEL==1 (
+            echo Good choice
+            REM git push --set-upstream %%r %_cBranch%
+        )
+        
+    )
+) else (
+    echo Will push to remote %1
+    pause
+    git push --set-upstream %1 %_cBranch%
+)
