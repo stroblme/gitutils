cacedc1dd2571e1ae62025f7b0f740fcd662cd8f
m17538 <melvin.strobl@microchip.com>
2019-05-10 09:02:24 +0200

    added remote checking in pushh

--- git-pushh.cmd
+++ git-pushh.cmd
@@ -1,2 +1,14 @@
 @echo off
-git push --set-upstream %1 master
+
+if "%~1"=="" goto remoteProcessing else goto parameterProcessing
+
+:remoteProcessing
+FOR /F %%I IN ('git remote') DO SET var=%%I
+
+for %%i in (%var%) do  (
+    echo %%i
+)
+
+:parameterProcessing
+echo %1
+REM git push --set-upstream %1 master
