c988494db1d2105a2f052c5166086892cabfe199
Melvin Strobl <melvin.strobl@web.de>
2019-08-17 21:44:56 +0200

    added alternative registering

---
+++ register_alt.cmd
@@ -0,0 +1,5 @@
+ git config --global alias.pushh "!C:\\PROGRA~1\\Git\\cmd\\git-pushh.cmd"
+ git config --global alias.committ "!C:\\PROGRA~1\\Git\\cmd\\git-committ.cmd"
+ git config --global alias.logg "!C:\\PROGRA~1\\Git\\cmd\\git-logg.cmd"
+ git config --global alias.mergee "!C:\\PROGRA~1\\Git\\cmd\\git-mergee.cmd"
+ git config --global alias.cleanupp "!C:\\PROGRA~1\\Git\\cmd\\git-cleanupp.cmd"
