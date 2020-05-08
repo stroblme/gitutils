44f761a46d65c7f5c32e02bf6c4cd0c4324bbcb8
Melvin Strobl - M17538 <melvin.strobl@microchip.com>
2020-05-08 12:15:12 +0200

    merged register scripts

--- register.cmd
+++ register.cmd
@@ -1,10 +1,13 @@
-@echo off
-setlocal enabledelayedexpansion
+echo Using alternative register method
+register_alt.cmd
 
-for /r . %%f in (git-*.cmd) do (
-    set path=%%~sf 
+REM @echo off
+REM setlocal enabledelayedexpansion
 
-    set alias=%%~nf
+REM for /r . %%f in (git-*.cmd) do (
+REM     set path=%%~sf 
 
-    git config --global alias.!alias! "!path!"
-)
+REM     set alias=%%~nf
+
+REM     git config --global alias.!alias! "!path!"
+REM )
--- register_alt.cmd
+++ register_alt.cmd
@@ -3,3 +3,5 @@
  git config --global alias.logg "!C:\\PROGRA~1\\Git\\cmd\\git-logg.cmd"
  git config --global alias.mergee "!C:\\PROGRA~1\\Git\\cmd\\git-mergee.cmd"
  git config --global alias.cleanupp "!C:\\PROGRA~1\\Git\\cmd\\git-cleanupp.cmd"
+
+ echo Done!
