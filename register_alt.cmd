57ad036c21ca508c9f1b5c50554cc421bbff6b02
stroblme <melvin.strobl@web.de>
2021-06-08 22:15:40 +0200

    gc

--- register_alt.cmd
+++
@@ -1,8 +0,0 @@
- git config --global alias.pushh "!C:\\PROGRA~1\\Git\\cmd\\git-pushh.cmd"
- git config --global alias.committ "!C:\\PROGRA~1\\Git\\cmd\\git-committ.cmd"
- git config --global alias.logg "!C:\\PROGRA~1\\Git\\cmd\\git-logg.cmd"
- git config --global alias.mergee "!C:\\PROGRA~1\\Git\\cmd\\git-mergee.cmd"
- git config --global alias.cleanupp "!C:\\PROGRA~1\\Git\\cmd\\git-cleanupp.cmd"
- git config --global alias.amendee "!C:\\PROGRA~1\\Git\\cmd\\git-amendee.cmd"
-
- echo Done!
